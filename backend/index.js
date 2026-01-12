9c4fdc3858ade1767ab2cfd81c1ac7929d162010
yusufriswan <119690890+yusufriswan@users.noreply.github.com>
2026-01-12 21:25:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 //middleware 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://full-stack-book-store-tan.vercel.app'],
     credentials: true
 }))
 
